f6bfd95af6fa540787f9d9c01f2d1315e3d5ae98
tsumaethan <tsumaethan20@gmail.com>
2024-04-10 09:32:38 +0300

    index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+// Define function expression called divide
+const divide = function(a, b) {
+    return a / b;
+  }
+  
+  // Define arrow function called square
+  const square = (x) => {
+    return x * x;
+  }
+  
+  // Define arrow function called add
+  const add = (a, b) => {
+    return a + b;
+  }
+  
